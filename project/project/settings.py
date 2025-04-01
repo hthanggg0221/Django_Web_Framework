130328ce9aef41bf2825a0219f71ec5a1fd083ec
Hoàng Văn Thắng <Thang.HV235828@sis.hust.edu.vn>
2025-04-02 02:03:43 +0700

    update postgresql

--- project/project/settings.py
+++ project/project/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'hthanggg0221',
+        'PASSWORD': 'epilogue4u212',
+        'HOST': 'django-project.cbyiy024gm2e.ap-southeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
